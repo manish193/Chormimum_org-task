fcbec00820f4a73f6c2ad179a681f32de6522836
Deadpool <manishsharma199903@gmail.com>
2021-03-19 13:44:53 +0530

    Milestone-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const NavBar = () => (
+  <div className="navbar">
+    <h3>Sample Diagnostics App </h3><br/>
+    <Link to="/">System Data</Link>
+    <Link to="/diagnostic_manager">System Diagnostic Manager</Link>
+    <Link to="/system_state">System State</Link>
+    
+  </div>
+);
+
+const Template = (props) => (
+  <div>
+    <NavBar />
+    <p className="page-info">
+      {props.title}
+    </p>
+    
+  </div>
+);
+
+const SystemData = () => (
+  <Template title="System Data" status="SystemData"/>
+);
+
+const SystemDiagnosticManager = () => (
+  <Template title="System Diagnostic Manager" status="SystemDiagnosticManager"/>
+);
+const SystemState = () => (
+  <Template title="System State" status="SystemState"/>
+);
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={SystemData}/>
+          <Route path="/diagnostic_manager" component={SystemDiagnosticManager}/>
+          <Route path="/system_state" component={SystemState}/>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
